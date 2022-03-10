3292401a03396a8b0773235b4146a04918bd8fe6
Navid Babakhani <navid.babakhani@gmail.com>
2022-03-10 18:50:36 +0100

    Removed unrequired valid annotation

--- src/main/java/com/wcc/distance/controller/PostCodeController.java
+++ src/main/java/com/wcc/distance/controller/PostCodeController.java
@@ -4,16 +4,12 @@ import com.wcc.distance.controller.dto.Location;
 import com.wcc.distance.exception.PostCodeNotFoundException;
 import com.wcc.distance.model.Point;
 import com.wcc.distance.service.PostCodeService;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
 @RestController
-@Validated
 public class PostCodeController {
 
     private final PostCodeService postCodeService;
@@ -24,7 +20,7 @@ public class PostCodeController {
 
     @PostMapping("/update")
     public Location updatePostCode(@RequestParam String postalCode,
-                                   @RequestBody @Valid Point coordinates) {
+                                   @RequestBody Point coordinates) {
         if (postCodeService.updateCoordinates(postalCode, coordinates)) {
             return new Location(postalCode, coordinates.getLatitude(), coordinates.getLongitude());
         } else {
